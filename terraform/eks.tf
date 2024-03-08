1b7c450e839d6ed5a0609932e765243859733227
Muthuri Munene <5156758+bryan-munene@users.noreply.github.com>
2024-03-08 11:39:54 +0300

    minor updates to instance type and helm config (#4)
    Co-authored-by: Bryan Muthuri <c_bmuthuri@ojolabs.com>

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -27,16 +27,16 @@ module "eks" {
 
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
-    instance_types = ["t2.micro"]
+    instance_types = ["t2.medium"]
   }
 
   eks_managed_node_groups = {
     base = {
-      min_size     = 1
+      min_size     = 3
       max_size     = 3
       desired_size = 3
 
-      instance_types = ["t2.micro"]
+      instance_types = ["t2.medium"]
 
       capacity_type  = "ON_DEMAND"
     }
